61f329f01a00b6ef2dfdb926ef1b21b1f666a6c5
Nathan (Schools Account) <39419019+NathanCadieuBaker@users.noreply.github.com>
2018-05-18 13:10:34 -0700

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,3 @@
+<script>
+alert("Warning this page has cussing in it, why the warning? Because I don't want whining teachers complaing about language because of the name of the song or the "lovely" fun facts. Thanks, Hope you understand.");
+</script>
